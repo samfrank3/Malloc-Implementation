f21fb7e1c51e7c4937ac3047e40294e4e97b8322
alexmartin1722 <76964530+alexmartin1722@users.noreply.github.com>
2022-04-12 14:41:43 -0400

    Update mm.c

--- implicit/mm.c
+++ implicit/mm.c
@@ -71,6 +71,24 @@ team_t team = {
 #define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 static char *heap_listp;
 
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+    
+    /*Allocate an even number of words to maintain alignment*/
+    size = (words % 2) ? (words + 1) * WSIZE : words*WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    
+    /*Initialize free block header/footer and the epilogue header*/
+    PUT(HDRP(bp), PACK(size,0)); /*Free block header*/
+    PUT(FTRP(bp), PACK(size,0)); /*Free block footer*/
+    PUT(HDRP(NEXT_BLK(bp)), PACK(0,1)); /*New epilogue header*/
+    
+    /*Coalesce if the previous block was free*/
+    return coalesce(bp);
+}
+
 /*
  * mm_init - initialize the malloc package.
  */
