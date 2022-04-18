b3470cf766d5b3fe8aa72db7260d926428c50f15
alexmartin1722 <76964530+alexmartin1722@users.noreply.github.com>
2022-04-17 20:37:32 -0400

    Update mm.c

--- Segregated/mm.c
+++ Segregated/mm.c
@@ -210,6 +210,18 @@ static void fill_block(void* current,size_t size){
     if((GET(GET_PREV(current)) == (size_t) NULL) && (GET(GET_NEXT(current)) != (size_t) NULL)){
         if(size <= 2){
             PUT((free_listp + 0), (size_t) GET(GET_NEXT(current)));
+        }else if(size <= 4){
+            PUT((free_listp + 8), (size_t) GET(GET_NEXT(current)));
+        }else if(size <= 8){
+            PUT((free_listp + 16), (size_t) GET(GET_NEXT(current)));
+        }else if(size <= 16){
+            PUT((free_listp + 24), (size_t) GET(GET_NEXT(current)));
+        }else if(size <= 32){
+            PUT((free_listp + 32), (size_t) GET(GET_NEXT(current)));
+        }else if(size <= 64){
+            PUT((free_listp + 40), (size_t) GET(GET_NEXT(current)));
+        }else{
+            PUT((free_listp + 48), (size_t) GET(GET_NEXT(current)));
         }
     }
     /*if(GET_PREV(current)==NULL){
